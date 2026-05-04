fda4863ca30aba112fe994ec424ea544f406bb98
Rufis72 <Milo.games.silverstein@gmail.com>
2026-05-03 18:24:12 -0700

    Finished FEN function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::io::empty;
-
 fn main() {
     let pos = Position::new();
     let fen = pos.to_fen();
-    println!("{fen}")
+    let test = 3 / 2;
+    println!("{test}");
+    println!("{fen}");
 }
 
 enum Piece {
@@ -48,7 +48,13 @@ struct Position {
     can_light_castle_queen_side: bool,
 
     // other misc variables
-    turn_count: u8,
+    /// how many moves have been made, increments after each player's move
+    turn_count: usize,
+    /// if a pawn has moved two squares, this is the square it moved over
+    en_pessant_square: Option<(u8, u8)>,
+    /// the number of halfmoves since the last capture or pawn advance
+    half_move_clock: usize,
+
 }
 
 impl Position {
@@ -165,7 +171,27 @@ impl Position {
             can_dark_castle_queen_side: true, 
             can_light_castle_king_side: true, 
             can_light_castle_queen_side: true, 
-            turn_count: 0 }
+            turn_count: 0,
+            en_pessant_square: None,
+            half_move_clock: 0
+        }
+    }
+
+    /// Turns a coordinate pair for a square into algebraic notation
+    /// 
+    /// This means (0, 0) becomes A1, (7, 7) becomes H8
+    fn coordinate_pair_to_algebraic_notation(coordinate_pair: (u8, u8)) -> String {
+        match coordinate_pair.0 {
+            0 => "A",
+            1 => "B",
+            2 => "C",
+            3 => "D",
+            4 => "E",
+            5 => "F",
+            6 => "G",
+            7 => "H",
+            _ => "Invalid number given",
+        }.to_string() + &coordinate_pair.1.to_string()
     }
 
     /// Returns the piece at a square
@@ -211,7 +237,8 @@ impl Position {
         // otherwise, we add the count of empty squares first (if it's not 0), then the letter for the piece
         // then at the end of the row, we add the count if it's not 0
         let mut fen: String = String::new();
-        for y in 0..8 {
+        // the .rev is because the fen starts at row 8, and goes down
+        for y in (0..8).rev() {
             let mut empty_square_count: usize = 0;
             for x in 0..8 {
                 match self.get_piece_at_square(x, y) {
@@ -252,10 +279,53 @@ impl Position {
             }
 
             // adding a / to the end if it's not the last time
-            if y != 7 {
+            if y != 0 {
                 fen += "/";
             }
         }
+
+        // next we add the player to move
+        if self.turn_count % 2 == 0 {
+            fen += " w"
+        } else {
+            fen += " b"
+        }
+
+        // then castling ability
+        // if neither player can castle, this is "-", otherwise, 
+        // it's K for light king side castle, Q, for light queen side, k for dark king side, etc
+        if !(self.can_dark_castle_king_side && self.can_dark_castle_queen_side && self.can_light_castle_king_side && self.can_light_castle_queen_side) {
+            fen += " -";
+        } else {
+            // adding the space for this field
+            fen += " ";
+
+            // now adding the characters for castling
+            if self.can_light_castle_king_side {
+                fen += "K";
+            }
+            if self.can_light_castle_queen_side {
+                fen += "Q";
+            }
+            if self.can_dark_castle_king_side {
+                fen += "k";
+            }
+            if self.can_dark_castle_queen_side {
+                fen += "q";
+            }
+        }
+
+        // adding en pessant stuff
+        fen +=  &(match self.en_pessant_square {
+            Some(T) => " ".to_owned() + &Self::coordinate_pair_to_algebraic_notation(T),
+            None => " -".to_owned()
+        });
+
+        // adding the halfmove clock
+        fen += &format!(" {}", self.half_move_clock);
+
+        // adding full move clock
+        fen += &format!(" {}", self.turn_count + 1);
 
         // returning the finished fen
         fen.to_string()
